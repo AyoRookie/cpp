eeae28536ad464183c5baec7dc4b51fd03a66366
Shihuaihzong <1042250769@qq.com>
2020-01-31 00:13:27 +0800

    哈哈哈哈成功

---
+++ 1.19/1.19/1.19.cpp
@@ -0,0 +1,21 @@
+﻿// 1.19.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a = 0;
+	int i = 0;
+	if (cin >> a)
+	{
+		if(cin>>i)
+			while (a<=i)
+			{
+				cout << a << " ";
+				a++;
+			}
+}
+	return 0;
+}
